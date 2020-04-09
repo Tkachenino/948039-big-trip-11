aa6bf2c427c3afdbaf4574021be3bbeb8ce9a9c3
Maxim Tkachenko <tkachenkonino1996@gmail.com>
2020-04-09 17:18:26 +0300

    фиксит картинки

--- src/components/pointEventTemplate.js
+++ src/components/pointEventTemplate.js
@@ -22,7 +22,7 @@ export const createPointEventTeplate = (trip) => {
     `<li class="trip-events__item">
     <div class="event">
       <div class="event__type">
-        <img class="event__type-icon" width="42" height="42" src="img/icons/${event}.png" alt="Event type icon">
+        <img class="event__type-icon" width="42" height="42" src="img/icons/${event.toLowerCase()}.png" alt="Event type icon">
       </div>
       <h3 class="event__title">${event} ${isMoveCheck} ${city}</h3>
 
